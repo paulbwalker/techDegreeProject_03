40acbed5b729ee135a0bcf4c6572e2b82caf098c
Paul Walker <pw97960@gmail.com>
2018-03-06 15:45:37 -0800

    Added the function to display and removes input if other is selected.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 // Wrap content in a jQuery ready function.
 $(function () {
-	console.log('hello world!');
+	// When user selects the other in the drop-down menu the 'other' input field displays
+	$('#title').on('change', function() {
+		const $jobRole = $('#title option:selected').val();
+		
+		if( $jobRole === 'other' ) {
+		// Removes the class to display the input
+			$('#other-title').removeClass('hide-other');
+		} else {
+		// Adds the class to hide the input
+			$('#other-title').addClass('hide-other');
+		}
+	});
+
 });
