0b4c7efc1626ca49000da44db861aef5771feaa6
Paul Walker <pw97960@gmail.com>
2018-03-10 06:26:30 -0800

    Refine changes to the T-shirt section.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,50 @@
-// Wrap content in a jQuery ready function.
-$(function () {
-	// When user selects the other in the drop-down menu the 'other' input field displays
-	$('#title').on('change', function() {
-		const $jobRole = $('#title option:selected').val();
-		
-		if( $jobRole === 'other' ) {
-		// Removes the class to display the input
-			$('#other-title').removeClass('hide-other');
+// I used the jQuery ready event function in case the script was moved.
+$(function() {
+
+/****************************** JOB ROLE SECTION *********************************/
+
+	let $designOptionShow =	$('#colors-js-puns').removeClass('hide-selection');
+	// This function uses the select:selected pseudo class and removes the class 'hide-selection'
+	//  if the value matches with 'other' option then it adds the class else it removes that class.
+	const selectField = () => {
+		let $selectedOption = $('#title option:selected').val();
+
+		if ( $selectedOption === 'other' ) {
+			// the removeClass is used because the CSS has a visibility: hidden.
+			$('#other-title').removeClass('hide-selection');
 		} else {
-		// Adds the class to hide the input
-			$('#other-title').addClass('hide-other');
-		}
-	});
-// Remove select options in the optgroup
-	const $jsPuns = $('#puns').remove();
-	const $jsHeart = $('#hearts').remove();
-
-// This function allows you to select which optgroup you want to see 
-// and displays which groupping you want to display
-	const toggleFields = () => {
-		$('#color optgroup').remove();
-
-		if ($('#design').val() === 'js puns') {
-			$('#color').append($jsPuns);
-		}
-		if ($('#design').val() === 'heart js') {
-			$('#color').append($jsHeart);
+			// if it does not match 'other' then I addClass to the selected element.
+			$('#other-title').addClass('hide-selection');
 		}
 	};
 
-// When you select the theme you call the toggleFields function
-	$('#design').on('change', toggleFields);
+	$('#title').on('change', selectField);
 
-});
+/********************************* T-SHIRT INFO ***********************************/
+
+// Remove the color options in the Global scope.
+	const $colors = $('#color option').remove();
+
+// This function toggle between js puns and js heart when you select it and shows the proper color.
+	const toggleFields = () => {
+	// I used this as the Local scope for this function.
+		const $colors = $('#color option').remove();
+
+		if ($('#design').val() === 'js puns') {
+			$('#color').append(`
+			  	<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+              	<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
+              	<option value="gold">Gold (JS Puns shirt only)</option> 
+          	`);
+		}
+		if ($("#design").val() === 'heart js') {
+			$('#color').append(`
+				<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+              	<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
+              	<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> 
+			`);
+		}
+	};
+
+// Calls the function on the js puns or js heart. 
+	$('#design').on('change', toggleFields);
