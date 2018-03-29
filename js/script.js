69068eb1f1b80f43eaedfdadf579aeed4f82ed72
Paul Walker <pw97960@gmail.com>
2018-03-29 08:33:50 -0700

    Add RegEx for the Credit Card Number Field.

--- js/script.js
+++ js/script.js
@@ -248,7 +248,7 @@ const enableFastFeedback = (formElement) => {
 		event.preventDefault();
 
 		if ( !isValidCCNum(ccNum) ) {
-	    	$('.user-ccNum').text('Need 16 digits.');		
+	    	$('.user-ccNum').text('Need 13 - 16 digits.');		
 		} else {
 			$('.user-ccNum').text('');
 	  	}
@@ -367,7 +367,7 @@ const isValidEmail = (email) => {
 	return email.length >= 4 && /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(email);
 };
 const isValidCCNum = (ccNum) => {
-	return ccNum.length >= 16; 
+	return ccNum.length >= 13 && /\b\d{13,16}\b/.test(ccNum); 
 };
 const isValidZip = (zip) => {
 	return zip.length >= 5 && /^\d{5}$|^\d{5}-\d{4}$/.test(zip);
