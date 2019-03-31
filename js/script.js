b54de5e311269b037a7466ee68c116945b8787f2
Paul Walker <pw97960@gmail.com>
2019-03-31 11:00:32 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,71 +4,72 @@ FSJS project 3 - Interactive Forms
 By Paul B. Walker
 ******************************************/
 
-
-// I used the jQuery ready event function in case the script was moved.
+// Another way to use the document.ready see learn.jquery.com/using-jquery-core document-ready
 $(function() {
 
-/****************************** JOB ROLE SECTION *********************************/
+  // jQuery method that focus on the name input text field.
+  $('#name').focus();
 
-	$('#other-title').hide(); // Hides the 'other' job role unless it is selected;
 
-	// This function uses the select:selected pseudo class and removes the class 'is-hidden'	
-	//  if the value matches with 'other' option then it adds the class else it removes that class.
-	const selectField = () => {
+/* -------- JOB ROLE SECTION ---------------------------------------------------------- */
+
+	  // The other jobs input fields is hidden until the user selects other on the form option.
+		$('#other-title').hide();
+
+  // This function shows or hides the select field depending of user selection
+  const selectField = () => {
 		let $selectedOption = $('#title option:selected').val();
 
 		if ( $selectedOption === 'other' ) {
-			// the removeClass is used because the CSS has a visibility: hidden.
+			// User selects other option in the dropdrown menu shows the other input filed.
 			$('#other-title').show();
 		} else {
-			// if it does not match 'other' then I addClass to the selected element.
+			// User selects something other than 'other' on the dropdown menu.
 			$('#other-title').hide();
 		}
 	};
 
+  // Calls the function when other is selected on the options menu.
 	$('#title').on('change', selectField);
 
-/********************************* T-SHIRT INFO ***********************************/
 
-// Remove the color options in the Global scope to disable the color dropdown menu.
-	const $colors = $('#colors-js-puns').addClass('is-hidden');
+/* -------- T-SHIRT INFO  --------------------------------------------------------------- */
 
- 
-// This function toggle between js puns and js heart when you select it and shows the proper color.
-	const toggleFields = () => {
-	// I used this as the Local scope for this function.
-		$colors.removeClass('is-hidden');
-		const $designs = $('#design option').first().addClass('is-hidden');
-		const $themes = $('#color option').remove();
+// Hide the color dropdown menu.
+$('#color').hide();
 
-		if ($('#design').val() === 'js puns') {
-			$('#color').append(`
-			  	<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
-              	<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
-              	<option value="gold">Gold (JS Puns shirt only)</option> 
-          	`);
-		} else if ( $("#design").val() === 'heart js') {
-			$('#color').append(`
-				<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
-              	<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
-              	<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> 
+/** The jQuery hide and show method did not yeild the results needed the first-child
+ * would appear first even when selecting the heart js. This was the best way to avoid it. */ 
+ const selectTheme = () => {
+
+	$('#color').show();
+
+ // To append the color options I use the remove method and assign it to $colors variable.
+	 let $colors = $('#color option').remove();
+
+	 if ($('#design').val() === 'js puns') {
+		 $('#color').append(`
+				<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
+				<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
+				<option value="gold">Gold (JS Puns shirt only)</option> 
 			`);
-		} else {
-			$('#color').append(`
-			  	<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option>
-              	<option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option> 
-              	<option value="gold">Gold (JS Puns shirt only)</option> 
-          	`);
-		}
-	};
-	// Calls the function on the js puns or js heart.
-	$('#design').on('change', toggleFields);
+	 } else if ( $("#design").val() === 'heart js') {
+		 $('#color').append(`
+				<option value="tomato">Tomato (I &#9829; JS shirt only)</option>
+				<option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option> 
+				<option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option> 
+		 `);
+	 } else {
+		$('#color').hide();
+	 }
+ };
+ // Calls the function on the js puns or js heart.
+ $('#design').on('change', selectTheme);
 
-
-/*   *****************    REGISTER FOR ACTIVITIES Cost *******************    */
+/* -------- ADD TOTAL REG FOR ACTIVIES  -------------------------------------------------- */
 
 // Append span tag with the input field on the .activities page using unobtrusive javascript.
-	$('.activities').append(`<span class="total-sum"><input type="text" name="amount" class="amount"></span>`);
+$('.activities').append(`<span class="total-sum"><input type="text" name="amount" class="amount"></span>`);
 
 // Hide the span when page loads
 	$('.total-sum').addClass('is-hidden');	
@@ -95,7 +96,8 @@ $(function() {
 	  $('input[type=checkbox]').on('click', addSum);
 
 
-/*    **************** REGISTER FOR ACTIVITIES Time Conflict ***************     */
+/* -------- TIME CONFLICT REG FOR ACTIVIES  ------------------------------------------- */
+
 
 // This function selects the label or deselect the same time the workshop is held.
 	const timeConflict = (workshopName, Workshop) => {
@@ -144,249 +146,172 @@ $(function() {
 // Calls the sameTime function when the checkbox is checked.
 	  $('input[type=checkbox]').on('click', sameTime);
 
-/*   *****************         PAYMENT INFO SECTION       *******************    */
-
-// // Add the class to the select payment to hide that selection.
-// 	const $creditCard = $('div#credit-card').addClass('is-hidden');
-
-// Assign variables to retrieve the element when called.
-	const $byCredit = $('#payment option').eq(1).val();
-	const $payPal = $('div p').eq(0).addClass('is-hidden');		
-	const $bitCoin = $('div p').eq(1).addClass('is-hidden');
-
-// This function shows what payment the users has selected. 
-	const makePayment = () => {
-		if ( $('#payment option:selected').val() === 'paypal' ) {
-			$payPal.removeClass('is-hidden');
-			// $payPal.is(true);
-			$bitCoin.addClass('is-hidden');
-			$creditCard.addClass('is-hidden');
-		} else if ( $('#payment option:selected').val() === 'bitcoin' ) {
-			$bitCoin.removeClass('is-hidden');
-			// $bitCoin.is(true);
-			$creditCard.addClass('is-hidden');
-			$payPal.addClass('is-hidden');
-		} else {
-			$creditCard.addClass('is-hidden');
-			$payPal.addClass('is-hidden');
-			$bitCoin.addClass('is-hidden');
-		}
-	};
 
 
-// Assign a handler to the select option dropdown menu
-	$('#payment').on('change', makePayment);
+/* -------- SELECT PAYMENT OPTION  --------------------------------------------------------- */
 
-/*   *****************         VALIDATE FORM      *******************    */	
+const $payOption = $('#payment');
+const $creditCard = $('#credit-card');
+const $payPal = $('p:contains("PayPal")');
+const $bitcoin = $('p:contains("Bitcoin")');
 
-// Assign the form into a jquery variable.
-	const $form = $('form');
-// Call the fast feedback.
-	enableFastFeedback($form);
+// function that only displays corresponding payment method
+// CC is first by default
+$payPal.hide();
+$bitcoin.hide();
+$payOption.val("credit card").attr("selected", true);
 
-// Submit form if all validation is correct.
-	$form.submit(function(event) { 
-		const name = $('#name').val();
-		const email = $('#mail').val();
-		const isChecked = $('.activities input[type="checkbox"]').is(':checked');
-		const ccNum = $('#cc-num').val();
-		const zip = $('#zip').val();
-		const cvv = $('#cvv').val();
-		const payPal = $('#payment option:selected').val();
-		const bitCoin = $('#payment option:selected').val();
+$payOption.change(function(){
 
-		validateNameField( name, event );
-		validateEmailField( email, event );
-		validateCheckboxField ( isChecked, event );
-		validateCCNumField( ccNum, event );
-		validateZipField( zip, event );
-		validateCvvField( cvv, event );
+  if ($(this).val() == "credit card") {
+    $creditCard.show();
+    $bitcoin.hide();
+    $payPal.hide();
+  } else if ($(this).val() == "paypal") {
+      $payPal.show();
+      $bitcoin.hide();
+      $creditCard.hide();
+  } else if ($(this).val() == "bitcoin") {
+      $bitcoin.show();
+      $payPal.hide();
+      $creditCard.hide();
+  } else if ($(this).val() == "select_method") {
+      $payPal.hide();
+      $bitcoin.hide();
+      $creditCard.hide();
+  }
+});
 
-	}); // Close the form function
-}); // closing the jQuery ready function.
 
-// This section is the variables for the fast feedback form submission.
-const enableFastFeedback = (formElement) => {
-	const nameInput  = formElement.find('#name');
-	const emailInput = formElement.find('#mail');
-	const checkboxInput = formElement.find('.activities legend');
-	const ccNumInput = formElement.find('#cc-num');
-	const zipInput   = formElement.find('#zip');
-	const cvvInput   = formElement.find('#cvv');
+/* -------- FORM SUBMIT  ------------------------------------------------------------------ */
 
-// This section enables the fast feedback on certain input before the form submission.
-	nameInput.blur(function(event) {
-		let name = $(this).val();
-		validateNameField(name, event);
+$('form').on('submit',function (event) {
+	const name = $('#name').val();
+	const email = $('#mail').val();
+	const checked = $('input[type="checkbox"]').is(':checked');
+	const ccNum = $('#cc-num').val();
+	const zip = $('#zip').val();
+	const cvv = $('#cvv').val();
+	const selectedCard = $('#payment>option').eq(1);
 
-		if ( !isValidName(name) ) {
-		    	$('.user-name').text('Please enter at least two characters.');
-		} else {
-				$('.user-name').text('');
-			   }
-	});
-	emailInput.blur(function(event) {
-		let email = $(this).val();
-		validateEmailField(email, event);
 
-		if ( !isValidEmail(email) ) {
-	    	$('.user-email').text('Please enter a valid email address.');
-		} else {
-			$('.user-email').text('');
-	  	}
-	});
-	checkboxInput.on('click', function(event) {
-		let isChecked = $(this).is(":checked");
-		event.preventDefault();
-		validateCheckboxField( checkbox, event );
-		if (!isChecked) {
-			$('.activities legend').addClass('error');		
-		} else {
-			$('.activities legend').removeClass('error');
-	  	}
-		
-	});
-	ccNumInput.blur(function(event) {
-		let ccNum = $(this).val();
-		validateCCNumField(ccNum, event);
-		event.preventDefault();
-
-		if ( !isValidCCNum(ccNum) ) {
-				$('.user-ccNum').text('Need 13 - 16 digits.');
-				$('.payment-info legend').addClass('error');	
-		} else {
-			$('.user-ccNum').text('');
-			$('.payment-info legend').removeClass('error');	
-	  	}
-	});
-	zipInput.blur(function(event) {
-		let zip = $(this).val();
-		validateZipField(zip, event);
-		event.preventDefault();
-
-		if ( !isValidZip(zip) ) {
-				$('.user-zip').text('Need 5 digits.');
-				$('.payment-info legend').addClass('error');
-		} else {
-			$('.user-zip').text('');
-			$('.payment-info legend').removeClass('error');	
-	  	}
-	});
-	cvvInput.blur(function(event) {
-		let cvv = $(this).val();
-		validateCvvField(cvv, event);
-		event.preventDefault();
-
-		if ( !isValidCvv(cvv) ) {
-				$('.user-cvv').text('Need 3 digits.');
-				$('.payment-info legend').addClass('error');
-		} else {
-			$('.user-cvv').text('');
-			$('.payment-info legend').removeClass('error');	
-	  	}
-	});
-
-}; // This is the end of the fast form input validation.
-
-// Validate form input function on neccessary inputs. 
-const validateNameField = (name, event) => {
-	if ( !isValidName(name) ) {
-	    $('#name').addClass('invalid');
-		$('label[for="name"]').addClass('error');
-		event.preventDefault();
+	if (selectedCard.is(':selected') ) {
+		validateCreditCard(ccNum, event);
+		validateZip(zip, event);
+		validateCvv(cvv, event);
+		validateName(name, event);
+		validateEmail(email, event);
+		validateActivities(checked, event);
 	} else {
-		$('#name').removeClass('invalid');
-		$('label[for="name"]').removeClass('error');
+		validateName(name, event);
+		validateEmail(email, event);
+		validateActivities(checked, event);
 	}
-};
-const validateEmailField = (email, event) => {
-	if ( !isValidEmail(email) ) {
-	    $('#mail').addClass('invalid');
-		$('label[for="mail"]').addClass('error');
-		event.preventDefault();
-	} else {
-		$('#mail').removeClass('invalid');
-		$('label[for="mail"]').removeClass('error');
-	}
-};
-const validateCheckboxField = ( isChecked, event ) => {
-	if (!isChecked) {
-		$('.activities legend').addClass('error');
-		event.preventDefault();
-	} else {
-		$('.activities legend').removeClass('error');
-	}
-};
-const validateDropdownField = ( isSelected, event ) => {
-	let $selectMethod = $('#payment option:selected').val();
-	const $varifyCard = validateCCNumField + validateZipField + validateCvvField;
-	 if ($selectMethod === 'credit card') {
-		return $varifyCard;
-	} else if ($selectMethod === payPal) {
-		return true;
-	} else if ($selectMethod === bitCoin) {
-		return true;
-	} else {
-		$('.payment-info legend').removeClass('error');
-		return true;
-	} 
-};											
-const validateCCNumField = ( ccNum, event ) => {
-	if ( !isValidCCNum(ccNum) ) {
-		$('#cc-num').addClass('invalid');
-		$('label[for="cc-num"]').addClass('error');
-		event.preventDefault();
-	} else {
-		$('#cc-num').removeClass('invalid');
-		$('label[for="cc-num"]').removeClass('error');
-	}
-
-};
-const validateZipField = ( zip, event ) => {
-	if ( !isValidZip(zip) ) {
-		$('#zip').addClass('invalid');
-		$('label[for="zip"]').addClass('error');
-		event.preventDefault();
-	} else {
-		$('#zip').removeClass('invalid');
-		$('label[for="zip"]').removeClass('error');
-	}
-
-};
-const validateCvvField = ( cvv, event ) => {
-	if ( !isValidCvv(cvv) ) {
-		$('#cvv').addClass('invalid');
-		$('label[for="cvv"]').addClass('error');
-		event.preventDefault();
-	} else {
-		$('#cvv').removeClass('invalid');
-		$('label[for="cvv"]').removeClass('error');
-	}
-
-};
+})
 
 
-// Varify inputs on specific RegEx.
-const isValidName = (name) => {
-	return name.length >= 2 && /^[a-zA-Z]+$/.test(name);
+/* -------- VALIDATE NAME FIELD ----------------------------------------------------------- */
+
+
+const validateName = (name, event) => {
+	if (!isValidName (name) ) {
+		$('.user-name').text(`Please enter name. Greater then 2. no numbers.`);	
+		event.preventDefault();	
+	} else {
+		$('.user-name').text('');
+	}
 }
+
+const validateEmail = (email, event) => {
+	if (!isValidEmail (email) ) {
+		$('.user-email').text(`Please enter your email.`);	
+		event.preventDefault();	
+	} else {
+		$('.user-email').text('');
+	}
+}
+
+const validateActivities = (checked, event) => {
+	if (!checked) {
+		$('.activities legend').addClass('error');
+		event.preventDefault();	
+		} else {
+		$('.activities legend').removeClass('error');
+		}
+};
+
+const validateCreditCard = (ccNum, event) => {
+	if (!isValidCreditCard (ccNum) ) {
+		$('.user-ccNum').text(`Please enter your credit card number.`);	
+		event.preventDefault();	
+	} else {
+		$('.user-ccNum').text('');
+	}
+}
+
+const validateZip = (zip, event) => {
+	if (!isValidZip (zip) ) {
+		$('.user-zip').text('Need zip code.');
+		event.preventDefault();
+	} else {
+		$('.user-zip').text('');
+	}
+}
+
+const validateCvv = (cvv, event) => {
+	if (!isValidCvv (cvv) ) {
+		$('.user-cvv').text('Need CVV number.');
+		event.preventDefault();
+	} else {
+		$('.user-cvv').text('');
+	}
+}
+
+
+
+
+/* -------- IS VALID FUNCTION -------------------------------------------------------------- */
+
+const isValidName = (name) => {
+	let regExName = /^[a-zA-Z]/;
+	if (name.length > 2 && name.match(regExName) ) {
+		return name;
+	}
+}
+
 const isValidEmail = (email) => {
-	return email.length >= 4 && /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(email);
-};
-const isValidCCNum = (ccNum) => {
-	return ccNum.length >= 13 && /\b\d{13,16}\b/.test(ccNum); 
-};
+	let regExMail = /^[a-z, 0-9]+@[^@.]+\.[a-z]/i;
+	if (email.match(regExMail) ) {
+		return email;
+	}
+}
+
+const isValidCreditCard = (ccNum) => {
+	let regExCcNum = /\d{14}|\d{16}/;
+	if (ccNum.match(regExCcNum) ) {
+		return ccNum;
+	}
+}
+
 const isValidZip = (zip) => {
-	return zip.length >= 5 && /^\d{5}$|^\d{5}-\d{4}$/.test(zip);
-};
+	let regExZip = /\d{5}|\d{5}-\d{4}/;
+	if (zip.match(regExZip) ) {
+		return zip;
+	}
+}
+
 const isValidCvv = (cvv) => {
-	return cvv.length === 3 && /[0-9]{3}$/.test(cvv);
-};
-const isValidPayPal = (paypal) => {
-	return true;
-};
-const isValidBitcoin = (bitcoin) => {
-	return true;
-};
+	let regExCvv = /\d{3}/;
+	if (cvv.match(regExCvv) ) {
+		return cvv;
+	}
+}
 
 
+
+
+
+
+
+
+}); // closing the jQuery ready function.
